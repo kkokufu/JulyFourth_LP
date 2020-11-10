8788ce9c867d2e8d670fd7cfc0f3d90522335550
Keita Kokufu <>
2020-11-11 01:10:46 +0900

    カレンダーのイベント登録機能に必要な関数をcontrollerで定義。 それらのhtml.erbファイルを作成。

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,45 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    Event.create(blog_parameter)
+    redirect_to blogs_path
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to blogs_path, notice:"削除しました"
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(blog_parameter)
+      redirect_to blogs_path, notice: "編集しました"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def event_parameter
+    params.require(:event).permit(:event_name, :category, :start_time)
+  end
+
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  post "events/:id/update" => "events#update"
+  get "events/:id/edit" => "events#edit"
+  post "events/create" => "events#create"
+  get "events/new" => "events#new"
   get 'events/index' => 'events#index'
+  get 'events/:id' => 'events#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
