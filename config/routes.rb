3ca35dd8be74e2b1df1fc5ac5feab78de8ba0d42
Keita Kokufu <>
2020-11-11 17:41:00 +0900

    LPにインスタグラマーの人の参加申し込みシステムを作るために postsクラス、テーブル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  
   post "events/:id/update" => "events#update"
   get "events/:id/edit" => "events#edit"
   post "events/create" => "events#create"
   get "events/new" => "events#new"
   get 'events/index' => 'events#index'
   get 'events/:id' => 'events#show'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
